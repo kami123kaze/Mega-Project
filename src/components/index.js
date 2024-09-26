5743b2182c66a00697b350548c475a65c703bf54
daksh chauhan <dakshchauhan2002@gmail.com>
2024-09-26 22:26:11 +0530

    made landing pages and login/register func--

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ import Footer from "./footer/Footer";
 import Container from "./Container";
 import Logo from "./Logo"
 import LogoutBtn from "./headers/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
 export{
-    Header,Footer,Container,Logo,LogoutBtn
+    Header,Footer,Container,Logo,LogoutBtn,Button,Input
 }
